ea4371f4417873fb8e3507c6b01c6ef144e3f908
Chris Scutcher <chris.scutcher@ninebysix.co.uk>
2015-04-21 17:52:09 +0100

    Fixed social links and blogroll

--- pelicanconf.py
+++ pelicanconf.py
@@ -20,14 +20,13 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = ()
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (
+    ('github profile', 'https://github.com/cscutcher'),
+    ('linkedin', 'https://uk.linkedin.com/in/cscutcher'),
+)
 
 DEFAULT_PAGINATION = 10
 
